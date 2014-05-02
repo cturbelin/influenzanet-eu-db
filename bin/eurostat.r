66d7266e37e8895e02df40f26b9ec2b7edd4193a
cturbelin <clement.turbelin@gmail.com>
2014-05-02 12:52:51 +0200

    R import script from eurostat data to generate csv data files

---
+++ bin/eurostat.r
@@ -0,0 +1,255 @@
+######
+# Generate geographic levels and population data files from EuroStat export
+# See doc/eurostat.md
+#####
+
+# Only keep countries
+library(reshape2)
+library(plyr)
+
+output.years = c(2011,2012,2013)
+
+countries = c("BE", "DK",  "ES", "FR", "IE", "IT", "NL", "PT", "SE", "UK")
+
+exclude.levels = c("Z+$","^FR9")
+
+# Load nuts levels
+nuts = read.csv2('data/eurostat/eurostat_nuts2_2010.csv')
+names(nuts) <- tolower(names(nuts))
+
+# Keep only levels in expected countries
+nuts = nuts[ nuts$country.code %in% countries, ]
+
+cat("Excluding zones\n")
+for(p in exclude.levels) {
+	i = grepl(p, nuts$nuts.code)
+	print(nuts[i,])
+	nuts = nuts[!i, ]
+}
+
+# Keep level 2 data
+nuts2 = nuts[ nuts$nuts.level == 2, c('nuts.code','country.code')]
+
+geo.levels = nuts2
+geo.levels = rename(geo.levels, list("nuts.code"="code_nuts2", "country.code"="country"))
+geo.levels$code_nuts2 = as.character(geo.levels$code_nuts2)
+geo.levels$code_nuts1 = substr(geo.levels$code_nuts2, 1, 3)
+
+write.csv2(geo.levels, file="data/nuts/geo_levels.csv", row.names=F)
+
+for(i in 1:3) {
+  g = nuts[ nuts$nuts.level == i, c('nuts.code','country.code', 'nuts.label')]
+  if(i > 1) {
+    g$upper = substr(g$nuts.code, 1, (i-1) + 2)
+    n = list(paste("code_nuts", i-1, sep=''))
+    names(n) <- 'upper'
+    g = rename(g, n)
+  }
+  g = rename(g, list("nuts.code"=paste("code_nuts",i, sep=''), 'country.code'="country", 'nuts.label'="title"))
+  write.csv2(g, file=paste("data/nuts/geo_nuts",i,".csv",sep=''),  row.names=F)
+}
+  
+  # Load demographic data 
+for(year in output.years) {  
+  
+  cat("Computing ", year,"\n")
+  d = read.csv("data/eurostat/eurostat_population_nuts2.csv")
+  names(d) <- tolower(names(d))
+  
+  d = d[ d$time <= year, ] # Remove future years
+  
+  # excluding znes
+  for(p in exclude.levels) {
+	i = grepl(p, d$geo)
+	print(d[i,])
+	d = d[!i, ]
+  }
+  
+  d$country = gsub("^([A-Z]{2}).*","\\1", d$geo)
+  
+  d = d[ d$country %in% countries, ]
+  
+  d$value = as.numeric(as.character(gsub(",","", d$value, fixed=T)))
+  
+  d$flag.and.footnotes = as.character(d$flag.and.footnotes)
+  
+  d$flag.and.footnotes = ifelse(d$flag.and.footnotes == "", NA, d$flag.and.footnotes)
+  
+  r = melt(d, id=c('time','geo','sex', 'age'), measure.vars=c('value'))
+  x1 = dcast(r, time + geo + age ~ sex, fun.aggregate=sum)
+  
+  # Get flags
+  r = melt(d, id=c('time','geo','sex', 'age'), measure.vars=c('flag.and.footnotes'))
+  x2 = dcast(r, time + geo + age ~ sex, fun.aggregate=function(x){x[1]})
+  # Assume that overall category flag (for sex) is the same as Female and Male ones
+  x2 = subset(x2, select=-c(F,M))
+  x2 = rename(x2, list("T"="flags"))
+  
+  x1 = merge(x1, x2, all.x=T)
+  x1 = rename(x1, list("F"="female","M"="male","T"="all"))
+  
+  x1 = merge(x1, nuts[, c('nuts.code', 'nuts.level', 'country.code')], by.x='geo', by.y='nuts.code', all.x=T)
+  
+  stopifnot( all(!is.na(x1$country.code)))
+  
+  x1.alllevels = x1
+  
+  # Keep only nuts2 level
+  x1 = x1[ x1$nuts.level == 2, ]
+  x1$geo = factor(x1$geo)
+  x1$country.code = factor(x1$country.code)
+  
+  # Get last available year for each country
+  y = as.list(by(x1, x1$geo, function(x) {
+        max(x$time[!is.na(x$all)], na.rm=T)
+      }))
+  y = data.frame(time=unlist(y), geo=names(y))
+  
+  if( !all(is.finite(y$time)) ) {
+      print( y[ !is.finite(y$time), ] )
+      cat("Régions sans données pour cette année - Impossible à calculer\n")
+      next()
+  }
+  
+  y = merge(y, nuts[ nuts$nuts.level == 2, c('nuts.code','country.code')], by.x='geo', by.y='nuts.code', all.x=T)
+  y$country.code = factor(y$country.code)
+  y$geo = factor(y$geo)
+  
+  available.years = tapply(y$time, y$country.code, function(x) length(unique(x)))
+  if(! all(available.years == 1)) {
+    
+    
+    stop("Some country has different year available for different levels")
+  }
+  
+  country.year = aggregate(time ~ country.code, data=y, min)
+  country.year = rename(country.year, list("time"="year.ref"))
+  
+  x1 = merge(x1, country.year, by='country.code', all.x=T)
+  
+  pop = x1[ x1$time == x1$year.ref, ]
+  
+  # Check that's ok
+  table(pop$country.code, pop$year.ref)
+  
+  pop$i = 1
+  check = aggregate(i ~ geo + age, data=pop, sum)
+  stopifnot(  all(check$i == 1) )
+  
+  max.age = 200
+  
+  pop$age.min = NA
+  pop$age.max = NA
+  
+  pop.allages = pop[ pop$age == "TOTAL", ]
+  
+  pop = pop[ !pop$age == "TOTAL", ]
+  pop$age = factor(pop$age)
+  
+  # Recode age group
+  i = grepl("^Y([0-9]+)\\-([0-9]+)", pop$age)
+  group = strsplit(gsub("^Y([0-9]+)\\-([0-9]+)","\\1;\\2", pop$age[i]), split=";",fixed=T)
+  group = do.call(rbind, lapply(group, function(x) { x = as.numeric(x); data.frame(min=x[1], max=x[2]) }))
+  
+  pop$age.min[i] = group$min
+  pop$age.max[i] = group$max
+  
+  i = pop$age == "Y_LT5"
+  pop$age.min[i] = 0
+  pop$age.max[i] = 4
+  
+  i = grepl("^Y_GE([0-9]+)", pop$age)
+  group = as.numeric(gsub("^Y_GE([0-9]+)","\\1", pop$age[i]))
+  pop$age.min[i] = group
+  pop$age.max[i] = NA
+  
+  # Select optimal bound (greatest age with all countries)
+  pop$bound = is.na(pop$age.max) & !is.na(pop$age.min)
+  
+  bounds = table(pop$country.code[pop$bound], pop$age.min[pop$bound])
+  bounds = bounds > 0
+  
+  # Which bound is available for all countries
+  bounds.all = apply(bounds, 2, all)
+  bounds.all = max(as.numeric(names(bounds.all)[which(bounds.all)]))
+  
+  cat("Selected bounds = ", bounds.all,"\n")
+  
+  # Remove other kind of bounds
+  pop$bounds.ok = pop$bound & pop$age.min == bounds.all
+  pop = pop[ !(pop$bound & !pop$bounds.ok), ]
+  
+  age.groups = unique(pop[, c('age','age.min', 'age.max')])
+  age.groups = age.groups[ order(age.groups$age.min), ]
+  
+  i = age.groups$age != "UNK"
+  age.groups$min.expected[i] = seq(0, bounds.all, by=5)
+  age.groups$max.expected[i] = age.groups$min.expected[i] + 5
+  age.groups$max.expected[ age.groups$min.expected == bounds.all] = NA
+  
+  # Check all expected groups are ok
+  stopifnot(all(age.groups$min[i] == age.groups$min.expected[i]))
+  
+  pop.frame = merge(data.frame(geo=nuts2$nuts.code), data.frame(age=age.groups$age))
+  
+  pop = merge(pop, pop.frame, by=c('geo','age'), all=T)
+  
+  if( any(is.na(pop$all)) ) {
+    print( pop[ is.na(pop$all)])
+    stop("Some population are unknown in expected geo levels or age groups")
+  }
+  
+  # Now that's ok
+  
+  pop = pop[ pop$age != "UNK", ]
+  
+  pop = pop[, c('geo','country.code','all','male','female','flags','year.ref', 'age.min','age.max')]
+  pop = rename(pop, list("geo"="code_nuts2","country.code"="country"))
+  
+  cat("Reference year for this year")
+  print(table(pop$country, pop$year.ref))
+  
+  write.csv(pop, file=paste("data/population/",year,"_pop_nuts2_age5.csv",sep=''), row.names=F)
+  
+  pop = merge(pop, geo.levels[, c("code_nuts2","code_nuts1")], by="code_nuts2", all.x=T)
+  
+  pop.nuts2 = pop
+  
+  p = aggregate(cbind(all, male, female) ~ code_nuts2, data=pop.nuts2, sum)
+  m = aggregate(cbind(year.ref) ~ code_nuts2, data=pop.nuts2 , min)
+  p = merge(p, m, by='code_nuts2', all.x=T)
+  p$country = substr(p$code_nuts2, 1, 2)
+  write.csv(p, file=paste("data/population/",year,"_pop_nuts2.csv",sep=''), row.names=F)
+  
+  p = aggregate(cbind(all, male, female) ~ code_nuts1 + age.min + age.max, data=pop, sum)
+  m = aggregate(cbind(year.ref) ~ code_nuts1, data=pop, min)
+  p = merge(p, m, by='code_nuts1', all.x=T)
+  if( any(!is.na(pop$flags)) ) {
+    m = aggregate(flags ~ code_nuts1, data=pop, function(x) { x = x[!is.na(x)]; ifelse(length(x)>0, x[1], "")})
+    p = merge(p, m, by='code_nuts1', all.x=T)
+  } else {
+    p$flags = NA
+  }
+  p$country = substr(p$code_nuts1, 1, 2)
+  
+  write.csv(p, file=paste("data/population/",year,"_pop_nuts1_age5.csv",sep=''), row.names=F)
+
+  pop.nuts1 = p
+
+  p = aggregate(cbind(all, male, female) ~ code_nuts1, data=pop.nuts1, sum)
+  m = aggregate(cbind(year.ref) ~ code_nuts1, data=pop.nuts1, min)
+  p = merge(p, m, by='code_nuts1', all.x=T)
+  p$country = substr(p$code_nuts1, 1, 2)
+  write.csv(p, file=paste("data/population/",year,"_pop_nuts1.csv",sep=''), row.names=F)
+  
+  p = aggregate(cbind(all, male, female) ~ country + age.min + age.max, data=pop.nuts1, sum)
+  m = aggregate(cbind(year.ref) ~ country, data=pop.nuts1, min)
+  p = merge(p, m, by='country', all.x=T)
+  write.csv(p, file=paste("data/population/",year,"_pop_country_age5.csv",sep=''), row.names=F)
+
+  p = aggregate(cbind(all, male, female) ~ country, data=pop.nuts1, sum)
+  m = aggregate(cbind(year.ref) ~ country, data=pop.nuts1, min)
+  p = merge(p, m, by='country', all.x=T)
+  write.csv(p, file=paste("data/population/",year,"_pop_country.csv",sep=''), row.names=F)
+  
+}
---
+++ doc/Eurostat.md
@@ -0,0 +1,35 @@
+Import Geographic levels & Population data form EuroStat
+=========================================================
+
+* Extract NUTS 2 levels & save it as CSV files ";" separaor
+
+Expected columns are
+
+ NUTS CODE;NUTS LABEL;NUTS LEVEL;COUNTRY CODE;COUNTRIES SORTING ORDER;ORDER
+
+* Extract population data with 5-year-age group and sex at NUTS 2 level, 
+	* All years from 2009 
+	* use "With code" option (avoid "label"), with Total
+	* Include all age groups (including total, unknown and sometimes several upper bounds)
+    * Inclde "Flag and Footnotes"
+	
+Expected columns are :
+ * "TIME" : Year of population
+ * "GEO" : NUTS 2 code
+ * "SEX" : "T","F","M" 
+ * "AGE" : Age group code for ex "Y45-49", "TOTAL", "UNK" (for unknown), Y_GE80
+ * "Value" : Population for the strata (English notation, "," as thousands separator)
+ * "Flag and Footnotes"
+ 
+Save it as CSV (";" sep) into data/eurostat/eurostat_population_nuts2.csv
+
+Run eurostat.r (in root dir)
+ * Will generate all files in data/nuts
+ * Will generate all population files
+	* total by for each geo levels
+	* age_group for each geo levels (country, nuts1, nuts2)
+	
+For each year from 2011-2013, pick the last available census available in each country (sometimes the last census could not be available for the current year).
+The year of the used census for each country will be registred in the "year_ref" column
+
+Some geo levels can be excluded (like overseas regions in France, counted in global population), since those population are not included in InfluenzaNet project yet.
