2749411787f9adc28f7b77be6d0aa8c870ecf44a
cturbelin <clement.turbelin@gmail.com>
2015-02-10 15:05:28 +0100

    cleanup country code (BN=>BE)

--- sql/import/cleanup_data.sql
+++ sql/import/cleanup_data.sql
@@ -1,4 +1,7 @@
 UPDATE epidb_results_intake set "country"=upper("country");
 UPDATE epidb_results_intake set "Q3"=upper("Q3");
 
+UPDATE epidb_results_intake set "country"='BE' where "country"='BN';
+UPDATE epidb_results_weekly set "country"='BE' where "country"='BN';
+
 CREATE INDEX idx_intake_q3_country ON epidb_results_intake USING btree (country, "Q3");
